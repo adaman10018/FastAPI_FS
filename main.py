5b39d645c78bde47b8a53974cf7b925aa92487ce
adaman10018 <adaman10018@gmail.com>
2024-10-06 13:30:49 +0300

    Summary and Description

--- main.py
+++ main.py
@@ -13,12 +13,24 @@ def index():
 # def get_all_blogs():
 #     return {'message': 'All blogs available'}
 
-@app.get('/blog/all', tags=['blog'])
+@app.get('/blog/all', 
+         tags=['blog'],
+         summary='Retrieve all Blogs',
+         description='This api call simulates fetching all blogs'
+        )
 def get_all_blogs(page=1, page_size: Optional[int]=10):
     return {'message': f'All {page_size} blogs on page {page}'}
 
 @app.get('/blog/{id}/comments/{comment_id}', tags=['blog','comment'])
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulates retrieving a comment of a blog
+
+    - **id** mandatory path parameter
+    - **comment_id** mandatory path parameter
+    - **valid** optional query parameter
+    - **username** optional query parameter
+    """
     return {'message': f'blog_id: {id}, comment_id: {comment_id}, valid: {valid}, username: {username}'}
 
 class BlogType(str, Enum):
