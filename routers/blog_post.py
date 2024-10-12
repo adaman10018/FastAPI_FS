24cbcbab715720e401b70df836460df9e74348e6
adaman10018 <adaman10018@gmail.com>
2024-10-12 21:26:07 +0300

    Parameter metadata

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from pydantic import BaseModel
 from typing import Optional
 
@@ -20,4 +20,23 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         'data': blog,
         'id': id,
         'version': version        
+    }
+
+@router.post('/new/{id}/comment')
+def create_comment(
+    blog: BlogModel, 
+    id: int, 
+    comment_id: int = Query(
+        None, 
+        title='id of the comment', 
+        description='Some description for comment_id',
+        alias='commentId',
+        deprecated=True
+    )
+):
+
+    return {
+        'blog': blog,
+        'id': id,
+        'comment_id': comment_id
     }
