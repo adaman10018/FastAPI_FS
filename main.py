2ead999949c8c37976eeb5cbc2fbe7c56bf0c1c0
adaman10018 <adaman10018@gmail.com>
2024-10-19 18:17:18 +0300

    Write data in database

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import blog_get, blog_post
+from routers import blog_get, blog_post, user
 from db import models
 from db.database import engine
 
@@ -7,6 +7,7 @@ from db.database import engine
 app = FastAPI()
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+app.include_router(user.router)
 
 @app.get('/hello')
 def index():
